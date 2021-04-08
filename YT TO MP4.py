e1f306f8364316c6c83076e41c89af234fb90240
Pinindu Tharushan <66949352+Pinindu-Tharushan@users.noreply.github.com>
2021-04-08 17:04:53 +0530

    Add files via upload

--- YT TO MP4.py
+++ YT TO MP4.py
@@ -3,7 +3,7 @@ import youtube_dl
 import os
 
 os.system("clear")
-print('''
+print('''\007
 
 \033[1;33m  _     _________   _______ ____     __  __ _____  _  _   
 \033[1;32m \ \   / /__   __| |__   __/ __ \   |  \/  |  __ \| || |  
@@ -13,17 +13,17 @@ print('''
 \033[1;32m    |_|     |_|       |_|  \____/   |_|  |_|_|       |_|
 
 \033[1;91mv1.4
-\033[1;36m =============================================\
+\033[1;36m =============================================\033[1;m
 \033[1;33m|         BEST YOUTUBE TO MP4 CONVERTER       |
-\033[1;36m =============================================\'''')
+\033[1;36m =============================================\033[00m''')
 
 print()
-print("[1] සිංහල ")
-print("[2] English ")
+print("\e[96m[1] සිංහල ")
+print("\e[96m[2] English ")
 print()
 print("ඔබගේ භාෂාව තෝරන්න: ")
 print("Select Your language: ")
-ya = input("[+]=====> ")
+ya = input("\e[5m[+]=====> ")
 print()
 
 x = ""
@@ -38,7 +38,7 @@ def sinhala():
     print("වීඩියෝ එක ඩවුන්ලෝඩ් වෙලා ඉවරයි....!")
     print("ඒක ඔබේ ඩිවයිස් එකට දාගන්න ඕන නං පහත ක්‍රමය අනුගමනය කරන්න.")
     print()
-    print("nano sinhala.txt")
+    print("\e[44m\e[7mnano sinhala.txt")
     print()
     print("ඉහත සදහන් ආකාරයට එම පාටය ඔබගේ terminal එකේ type කරන්න.")
     print()
@@ -53,7 +53,7 @@ def english():
     print("The video has been downloaded.")
     print("If you want to install it on your device, follow the step below.")
     print()
-    print("nano english.txt")
+    print("\e[44m\e[7mnano english.txt")
     print()
     print("Type the same color in your terminal as mentioned above.")
     print()
