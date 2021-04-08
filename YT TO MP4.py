577bf56be865f4cdf0bb76c0781ec27612104e85
Pinindu Tharushan <66949352+Pinindu-Tharushan@users.noreply.github.com>
2021-04-08 17:27:24 +0530

    Add files via upload

--- YT TO MP4.py
+++ YT TO MP4.py
@@ -10,19 +10,19 @@ print('''\007
 \033[1;33m  \ \_/ /   | |       | | | |  | |  | \  / | |__) ||| |_ 
 \033[1;32m   \   /    | |       | | | |  | |  | |\/| |  ___/__   _|
 \033[1;33m    | |     | |       | | | |__| |  | |  | | |      | |  
-\033[1;32m    |_|     |_|       |_|  \____/   |_|  |_|_|      |_|\033[1;91mv1.6
+\033[1;32m    |_|     |_|       |_|  \____/   |_|  |_|_|      |_|\033[1;91mv1.7
 
 \033[1;36m =============================================\033[1;m
 \033[1;33m|         BEST YOUTUBE TO MP4 CONVERTER       |
 \033[1;36m =============================================\033[00m''')
 
 print()
-print('''\e[96m[1] සිංහල ''')
-print('''\e[96m[2] English ''')
+print('''echo -e \e[96m[1] සිංහල ''')
+print('''echo -e \e[96m[2] English ''')
 print()
 print("ඔබගේ භාෂාව තෝරන්න: ")
 print("Select Your language: ")
-ya = input('''\e[5m[+]=====> ''')
+ya = input('''echo -e \e[5m[+]=====> ''')
 print()
 
 x = ""
@@ -37,7 +37,7 @@ def sinhala():
     print("වීඩියෝ එක ඩවුන්ලෝඩ් වෙලා ඉවරයි....!")
     print("ඒක ඔබේ ඩිවයිස් එකට දාගන්න ඕන නං පහත ක්‍රමය අනුගමනය කරන්න.")
     print()
-    print('''\e[44m\e[7mnano sinhala.txt''')
+    print('''echo -e \e[44m\e[7mnano sinhala.txt''')
     print()
     print("ඉහත සදහන් ආකාරයට එම පාටය ඔබගේ terminal එකේ type කරන්න.")
     print()
@@ -52,7 +52,7 @@ def english():
     print("The video has been downloaded.")
     print("If you want to install it on your device, follow the step below.")
     print()
-    print('''\e[44m\e[7mnano english.txt''')
+    print('''echo -e \e[44m\e[7mnano english.txt''')
     print()
     print("Type the same color in your terminal as mentioned above.")
     print()
